1009f1d19387d6a937f86f81ea8e5dd3c1cbf1c4
Ariele Bonifacio <arielebonifacio@gmail.com>
2023-03-27 15:58:02 +0200

    react-router for deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "./styles/themes/default";
 import { GlobalStyle } from "./styles/global"
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { Router } from "./Router";
 
 export function App() {
 
   return (
     <ThemeProvider theme={defaultTheme}>
-      <BrowserRouter>
+      <HashRouter>
             <Router />
         <GlobalStyle />
-      </BrowserRouter>
+      </HashRouter>
     </ThemeProvider>
   )
 }
